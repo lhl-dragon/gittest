1e86faf7f20c65a6d66427e585dc1598bbed9ebb
lhl-dragon <1360274272@qq.com>
2021-03-24 13:44:07 +0800

    克隆后第一次修改

--- learngit/src/git.java
+++ learngit/src/git.java
@@ -2,5 +2,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("ok");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("abc");
+        }
     }
 }
